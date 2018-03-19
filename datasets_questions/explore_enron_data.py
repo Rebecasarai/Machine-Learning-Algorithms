999c879ca0c6724462c8ecbee7f6e27a438e3964
Rebeca Saraí <callmebeca@gmail.com>
2018-03-19 22:08:45 +0100

    Starting Project

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -14,10 +14,52 @@
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
     
 """
-
+import os
+os.chdir("/Users/rebecagonzalez/Desktop/DeepLearning/ud120-projects/datasets_questions")
 
 import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
 
+print('Number of people in the Enron dataset: {0}'.format(len(enron_data)))
+
+print('Number of columnos per person in the Enron dataset: {0}'.format(len(enron_data.values()[0])))
+
+
+'''In other words, count the number of entries in the dictionary where
+data[person_name]["poi"]==1'''
+
+pois = [x for x, y in enron_data.items() if y['poi']]
+print 'Number of POI\'s: {0}'.format(len(pois))
+
+# DELETE ME
+print(enron_data['PRENTICE JAMES'])
+
+
+salary_count = 0
+for key, value in enron_data.items():
+    if value['director_fees'] != "NaN":
+        salary_count += 1;
+        print(value['email_address'],value['director_fees'])
+print("The number of people whose director fees is not null: %f " %salary_count)
+
+
+
+email_count = 0
+for key, value in enron_data.items():
+    if value['email_address'] != "NaN":
+        email_count += 1;
+print("The number of people whose email is not null: %f" %email_count)
+
+print(enron_data['COLWELL WESLEY'])
+
+
+
+
+print()
+print()
+
+names = ['SKILLING JEFFREY K', 'FASTOW ANDREW S', 'LAY KENNETH L']
+names_payments = {name:enron_data[name]['total_payments'] for name in names}
+print sorted(names_payments.items(), key=lambda x: x[1], reverse=True)
